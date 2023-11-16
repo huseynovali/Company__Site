9cae9d44adaf0e555469e7b237d93e7521831de8
User <huseynovali612@gmail.com>
2023-11-17 00:23:05 +0400

    create navbar

--- client/src/components/LayoutComp/Navbar.jsx
+++ client/src/components/LayoutComp/Navbar.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/Logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className='px-6 py-3 '>
-      <div className='logo'>
-        
+    <div className="px-6 py-3 my-[30px] relative z-10 flex justify-between items-center">
+      <Link to={"/"} className="logo ">
+        <img src={logo} alt="logo" />
+      </Link>
+      <div className="nav__links">
+        <ul className="flex">
+          <li className="  text-white rounded-[82px] hover:bg-[#262626]">
+            <Link className="text-sm py-[10px] px-4 block" to={"/"}>
+              Home
+            </Link>
+          </li>
+          <li className=" text-white rounded-[82px] hover:bg-[#262626]">
+            <Link className="text-sm py-[10px] px-4 block">Careers</Link>
+          </li>
+          <li className=" text-white rounded-[82px] hover:bg-[#262626]">
+            <Link className="text-sm py-[10px] px-4 block">About</Link>
+          </li>
+          <li className=" text-white rounded-[82px] hover:bg-[#262626]">
+            <Link className="text-sm py-[10px] px-4 block">Security</Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="auth__button">
+        <button className="mr-5 text-white">Sign Up</button>
+        <button className="px-6 py-3 bg-[#CAFF33] rounded-[82px]">Login</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Container from "../components/Container";
+import Navbar from "../components/LayoutComp/Navbar";
 
 function MainLayout() {
   return (
     <Container>
+      <Navbar/>
       Layout
       <Outlet />
     </Container>
