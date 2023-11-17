34b31cacf2941168d68602dc296cc4a5906438de
User <huseynovali612@gmail.com>
2023-11-18 01:01:23 +0400

    create our product section

---
+++ client/src/components/HomeComp/OurProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function OurProduct() {
+  return (
+    <div className='py-14'>
+        <p className='text-[38px] text-white'>Our <span className='text-[#CAFF33]'>Products</span></p>
+        <div>
+            <p></p>
+        </div>
+    </div>
+  )
+}
+
+export default OurProduct
--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import img1 from "../assets/Abstract Design.svg";
 import WlcComp from "../components/HomeComp/WlcComp";
+import OurProduct from "../components/HomeComp/OurProduct";
 function Home() {
   return (
     <div>
       <img src={img1} alt="" className="absolute inset-0" />
       <WlcComp/>
+      <OurProduct/>
     </div>
   );
 }
